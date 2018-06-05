9a5dd6e1479f34b6e65aa291ca16d02272e82aa3
Chris Vest <mr.chrisvest@gmail.com>
2018-06-05 14:38:53 +0200

    Update the licenser to a more modern version.
    This also makes it a comment instead of a javadoc block.
    Also, the mycila license header plugin has been added to the pom file, so checking and formatting can be done automatically now.

--- src/main/java/com/jvmtop/profiler/CPUSampler.java
+++ src/main/java/com/jvmtop/profiler/CPUSampler.java
@@ -1,13 +1,11 @@
-/**
+/*
  * jvmtop - java monitoring for the command-line
+ * Copyright © 2013 Patric Rufflar
  *
- * Copyright (C) 2013 by Patric Rufflar. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,8 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.jvmtop.profiler;
 
