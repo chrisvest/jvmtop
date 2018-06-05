0a823423c670c10266c676a0bf6226e646c14080
Chris Vest <mr.chrisvest@gmail.com>
2018-06-05 14:28:30 +0200

    Cleanup the VMProfileView file.

--- src/main/java/com/jvmtop/view/VMProfileView.java
+++ src/main/java/com/jvmtop/view/VMProfileView.java
@@ -26,81 +26,60 @@ import com.jvmtop.openjdk.tools.LocalVirtualMachine;
 import com.jvmtop.profiler.CPUSampler;
 import com.jvmtop.profiler.MethodStats;
 
-import java.util.Iterator;
-
 /**
  * CPU sampling-based profiler view which shows methods with top CPU usage.
  *
  * @author paru
- *
  */
-public class VMProfileView extends AbstractConsoleView
-{
+public class VMProfileView extends AbstractConsoleView {
 
-  private CPUSampler cpuSampler_;
+  private CPUSampler cpuSampler;
 
-  private VMInfo     vmInfo_;
+  private VMInfo vmInfo;
 
-  public VMProfileView(int vmid, Integer width) throws Exception
-  {
+  public VMProfileView(int vmid, Integer width) throws Exception {
     super(width);
-    LocalVirtualMachine localVirtualMachine = LocalVirtualMachine
-        .getLocalVirtualMachine(vmid);
-    vmInfo_ = VMInfo.processNewVM(localVirtualMachine, vmid);
-    cpuSampler_ = new CPUSampler(vmInfo_);
+    LocalVirtualMachine localVirtualMachine =
+        LocalVirtualMachine.getLocalVirtualMachine(vmid);
+    vmInfo = VMInfo.processNewVM(localVirtualMachine, vmid);
+    cpuSampler = new CPUSampler(vmInfo);
   }
 
   @Override
-  public void sleep(long millis) throws Exception
-  {
+  public void sleep(long millis) throws Exception {
     long cur = System.currentTimeMillis();
-    cpuSampler_.update();
-    while (cur + millis > System.currentTimeMillis())
-    {
-      cpuSampler_.update();
+    cpuSampler.update();
+    while (cur + millis > System.currentTimeMillis()) {
+      cpuSampler.update();
       super.sleep(100);
     }
-
   }
 
   @Override
-  public void printView() throws Exception
-  {
-    if (vmInfo_.getState() == VMInfoState.ATTACHED_UPDATE_ERROR)
-    {
-      System.out
-          .println("ERROR: Could not fetch telemetries - Process terminated?");
+  public void printView() throws Exception {
+    if (vmInfo.getState() == VMInfoState.ATTACHED_UPDATE_ERROR) {
+      System.out.println("ERROR: Could not fetch telemetries - Process terminated?");
       exit();
       return;
     }
-    if (vmInfo_.getState() != VMInfoState.ATTACHED)
-    {
+    if (vmInfo.getState() != VMInfoState.ATTACHED) {
       System.out.println("ERROR: Could not attach to process.");
       exit();
       return;
     }
 
     int w = width - 40;
-    System.out.printf(" Profiling PID %d: %40s %n%n", vmInfo_.getId(),
-        leftStr(vmInfo_.getDisplayName(), w));
+    System.out.printf(" Profiling PID %d: %40s %n%n",
+        vmInfo.getId(), leftStr(vmInfo.getDisplayName(), w));
 
-    // these are the spaces taken up by the formatting, the rest is usable
-    // for printing out the method name
-    w = width - (1 + 6 + 3 + 9 + 3 + 2);
-    for (Iterator<MethodStats> iterator = cpuSampler_.getTop(20).iterator(); iterator
-        .hasNext();)
-    {
-      MethodStats stats = iterator.next();
-      double wallRatio = (double) stats.getHits().get()
-          / cpuSampler_.getTotal() * 100;
-      if (!Double.isNaN(wallRatio))
-      {
-        System.out.printf(" %6.2f%% (%9.2fs) %s()%n", wallRatio, wallRatio
-            / 100d
-            * cpuSampler_.getUpdateCount() * 0.1d,
-                stats.getClassName() + "." + stats.getMethodName() );
+    for (MethodStats stats : cpuSampler.getTop(20)) {
+      double wallRatio = (double) stats.getHits().get() / cpuSampler.getTotal() * 100;
+      if (!Double.isNaN(wallRatio)) {
+        double sampleTime = wallRatio / 100d * cpuSampler.getUpdateCount() * 0.1d;
+        String qualifiedMethodName = stats.getClassName() + "." + stats.getMethodName();
+        System.out.printf(" %6.2f%% (%9.2fs) %s()%n",
+            wallRatio, sampleTime, qualifiedMethodName);
       }
     }
   }
-
 }
